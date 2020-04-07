c7b62e41b754fd6f867344fe4211c10a013e6b57
Pedro Tiago Gomes <pedrotgomes24@gmail.com>
2020-04-07 08:51:08 +0100

    Monitoring and metrics

---
+++ src/CoPaymentGateway/CoPaymentGateway/Helpers/ResponseTimeHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+
+using Prometheus;
+
+namespace CoPaymentGateway.Helpers
+{
+    public class ResponseTimeHelper
+    {
+        private readonly RequestDelegate _next;
+
+        public ResponseTimeHelper(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            HistogramConfiguration config = null;
+            var sw = Stopwatch.StartNew();
+            await _next(context);
+            sw.Stop();
+
+            var histogram =
+                Metrics
+                    .CreateHistogram(
+                        "api_response_time_seconds",
+                        "API Response Time in seconds",
+                        config);
+
+            histogram
+                .WithLabels(context.Request.Method, context.Request.Path)
+                .Observe(sw.Elapsed.TotalSeconds);
+        }
+    }
+}
--- src/CoPaymentGateway/CoPaymentGateway/Startup.cs
+++ src/CoPaymentGateway/CoPaymentGateway/Startup.cs
@@ -16,6 +16,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
+using Prometheus;
+
 using Serilog;
 
 namespace CoPaymentGateway
@@ -64,6 +66,10 @@ namespace CoPaymentGateway
             {
                 endpoints.MapControllers();
             });
+
+            app.UseHttpMetrics();
+            app.UseMetricServer();
+            app.UseMiddleware<ResponseTimeHelper>();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
